83f1057a67be979fdfe47865632dfd42dae80b76
Anil Lohar <anil.lohar@i2econsulting.com>
2026-08-19 19:41:45 +0530

    Event Handlers

--- fronted/src/Components/Search/Search.tsx
+++ fronted/src/Components/Search/Search.tsx
@@ -1,11 +1,14 @@
-import React, { JSX, useState } from 'react'
+import React, { ChangeEvent, JSX, useState, SyntheticEvent } from 'react'
 
 type Props = {}
 
 const Search: React.FC<Props> = (props: Props): JSX.Element => {
     const [search, setSearch] = useState<string>('');
-    const onclick = (e:any) => {
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
         setSearch(e.target.value);
+        console.log(e);
+    }
+    const onClick = (e: SyntheticEvent) => {
         console.log(e);
     }
   return (
@@ -14,8 +17,9 @@ const Search: React.FC<Props> = (props: Props): JSX.Element => {
         type="text"
         placeholder="Search..."
         value={search}
-        onChange={(e) => onclick(e)}
+        onChange={(e) => handleChange(e)}
       />
+      <button onClick={(e) => onClick(e)}>Search</button>
     </div>
   )
 }
