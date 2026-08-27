bf720aa2fe24b29d13c8c90654a37193846b96f5
Anil Lohar <anil.lohar@i2econsulting.com>
2026-08-27 16:43:28 +0530

    Data Flow

--- frontend/src/Components/CardList/CardList.tsx
+++ frontend/src/Components/CardList/CardList.tsx
@@ -1,13 +1,11 @@
 import React, { JSX } from 'react'
 import Card from '../Card/Card'
-import Search from '../Search/Search'
 
 interface Props {}
 
 const CardList: React.FC<Props> = (props: Props): JSX.Element => {
   return (
     <div>
-        <Search />
         <Card companyName='Apple' ticker='AAPL' price={100}  />
         <Card companyName='Microsoft' ticker='MSFT' price={200} />
         <Card companyName='Google' ticker='GOOGL' price={150} />
