82ae70cf60e57dec3b693ba7c16074b2a5fe2dec
Anil Lohar <anil.lohar@i2econsulting.com>
2026-09-06 14:46:47 +0530

    Arrays

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent, SyntheticEvent, useState } from 'react';
 import CardList from './Components/CardList/CardList';
+import ListPortfolio from './Components/Portfolio/ListPortfolio/ListPortfolio';
 import Search from './Components/Search/Search';
 import { CompanySearch } from './company';
 import { searchCompanies } from './api';
@@ -7,6 +8,7 @@ import { searchCompanies } from './api';
 
 function App() {
   const [search, setSearch] = useState<string>('');
+  const [portfolioValues, setPortfolioValues] = useState<string[]>([]);
   const [searchResult, setSearchResult] = useState<CompanySearch[]>([]);
   const [serverError, setServerError] = useState<string>('');
   const handleSearchChange = (e: ChangeEvent<HTMLInputElement>) => {
@@ -24,14 +26,19 @@ function App() {
     console.log(searchResult);
   }
 
-  const onPortfolioCreate = (e: SyntheticEvent) =>{
+  const onPortfolioCreate = (e: any) =>{
     e.preventDefault();
-    console.log(e);
+    const exists = portfolioValues.find(value => value === e.target[0].value);
+    if (exists) { return; }
+    const updatedPortfolio = [...portfolioValues, e.target[0].value];
+    setPortfolioValues(updatedPortfolio);
+
   }
 
   return (
     <div className="App">
       <Search onSearchSubmit={onSearchSubmit} search={search} handleSearchChange={handleSearchChange} />
+      <ListPortfolio portfolioValues={portfolioValues} />
       <CardList searchResults={searchResult} onPortfolioCreate={onPortfolioCreate} />
       {serverError && <h1>{serverError}</h1>}
     </div>
